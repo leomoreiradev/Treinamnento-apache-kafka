7db047cfffdc75defdaa35f4530d64c31dec54dc
Leandro Moreira <ilegra.oliveira@agi.com.br>
2022-12-18 18:21:56 -0300

    ErroHandler parte 2

--- str-consumer/src/main/java/com/leomoreiradev/io/strconsumer/excpetions/ErrorCustomHandler.java
+++ str-consumer/src/main/java/com/leomoreiradev/io/strconsumer/excpetions/ErrorCustomHandler.java
@@ -15,6 +15,10 @@ public class ErrorCustomHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
         log.info("EXCEPTION_HANDLER ::: Capturei um erro");
+        log.info("Payload :::  {}", message.getPayload());
+        log.info("Headers :::  {}", message.getHeaders());
+        log.info("Offset :::  {}", message.getHeaders().get("kafka_offset"));
+        log.info("message Exception :::  {}", e.getMessage());
         return null;
     }
 
