a3f586c2bc928247d461e618e58b414ef1f7557a
Leandro Moreira <ilegra.oliveira@agi.com.br>
2022-12-18 17:16:39 -0300

    custom listner

--- str-consumer/src/main/java/com/leomoreiradev/io/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/leomoreiradev/io/strconsumer/listeners/StrConsumerListener.java
@@ -1,15 +1,28 @@
 package com.leomoreiradev.io.strconsumer.listeners;
 
+import com.leomoreiradev.io.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1") //usando listener customizado
     public void create(String message) {
-        log.info("Receive  message {}", message);
+        log.info("CREATE ::: Receive  message {}", message);
     }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive  message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive  message {}", message);
+    }
+
 }
