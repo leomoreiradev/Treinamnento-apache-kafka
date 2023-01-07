f8beab008df3302f85607a9fcba1a1ac62e42f49
Leandro Moreira <ilegra.oliveira@agi.com.br>
2023-01-07 12:30:27 -0300

    Add producerConfig

--- payment-service/src/main/java/com/leomoreiradev/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/leomoreiradev/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,23 @@ package com.leomoreiradev.paymentservice.service.impl;
 
 import com.leomoreiradev.paymentservice.model.Payment;
 import com.leomoreiradev.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
 
+@RequiredArgsConstructor
 @Slf4j
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        log.info("Enviando o pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
